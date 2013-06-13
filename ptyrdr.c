2c3f4c7b41f32e75c0a0339a8e56d47317611bfe
YAMAMOTO Takashi <yamt@mwd.biglobe.ne.jp>
2013-06-13 23:09:33 +0900

    comment

--- ptyrdr.c
+++ ptyrdr.c
@@ -25,6 +25,17 @@
  * SUCH DAMAGE.
  */
 
+/*
+ * open a pty and dup it to stdin and stdout.
+ */
+
+/*
+ * this can be useful when creating a vpn with ssh and pppd.
+ * 
+ * an example:
+ * 	# pppd $(ptyrdr ssh peer_host pppd noauth) 10.0.0.1:10.0.0.2 noauth
+ */
+
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
